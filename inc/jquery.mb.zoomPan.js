9bf8f96d2bae831c1102d39bce3b7a22d8953ef4
Matteo Bicocchi <matteo@open-lab.com>
2011-11-02 16:14:07 +0100

    changed the way it moves: margin instead of position

--- inc/jquery.mb.zoomPan.js
+++ inc/jquery.mb.zoomPan.js
@@ -18,156 +18,150 @@
 
 (function($) {
 
-    $.zoomPan={
-        name:"mb.zoomPan",
-        author:"Matteo Bicocchi",
-        version:"1.0",
+  $.zoomPan={
+    name:"mb.zoomPan",
+    author:"Matteo Bicocchi",
+    version:"1.0",
 
-        defaults:{
-            startScale:random(.5),
-            minScale:1,
-            maxScale:2,
-            cWidth:900,
-            cHeight:900,
-            velocity:[12000,14000],
-            type:"linear",
-            display:"block"
-        },
-        init:function(opt){
-            return this.each(function(){
+    defaults:{
+      startScale:random(1,1.5),
+      minScale:1,
+      maxScale:2,
+      cWidth:900,
+      cHeight:900,
+      velocity:[12000,14000],
+      type:"linear",
+      display:"block"
+    },
 
-                var $el=$(this);
-                var el=$el.get(0);
-                el.opt=$.extend({},$.zoomPan.defaults,opt);
+    init:function(opt){
+      return this.each(function(){
 
-                $el.css({opacity:0});
-                $el.load(function(){
-
-                    //todo: Add height conditioning
-                    //first verify width and height, than add css
-                    if($el.width()<el.opt.cWidth){
-                        var ratio = el.opt.cWidth/$el.width();
-                        $el.css({width:el.opt.cWidth,height: $el.height()*ratio});
-                    }
-
-                    el.opt.oWidth=$el.width();
-                    el.opt.oHeight=$el.height();
-
-                    el.opt.cWidth=el.opt.cWidth?el.opt.cWidth:el.opt.oWidth;
-                    el.opt.cHeight=el.opt.cHeight?el.opt.cHeight:el.opt.oHeight;
-
-                    el.opt.cWidth=$el.data("width")?$el.data("width"):el.opt.cWidth;
-                    el.opt.cHeight=$el.data("height")?$el.data("height"):el.opt.cHeight;
-
-                    el.opt.maxScale=el.opt.maxScale-el.opt.minScale;
-
-                    $el.css({width:$el.width()*el.opt.startScale,height:$el.height()*el.opt.startScale, top:0, left:0});
+        var $el=$(this);
+        var el=$el.get(0);
+        el.opt=$.extend({},$.zoomPan.defaults,opt);
 
 
+        $el.load(function(){
+          if($el.width()<el.opt.cWidth){
+            var ratio = el.opt.cWidth/$el.width();
+            $el.css({width:el.opt.cWidth,height: $el.height()*ratio});
+          }
 
-                    var imgWrapper=$("<div/>").css({overflow:"hidden", width:el.opt.cWidth,height:el.opt.cHeight, position:"relative", display:el.opt.display, margin:0});
-                    $el.css({position:"absolute"}).wrap(imgWrapper);
+          el.opt.oWidth=$el.width();
+          el.opt.oHeight=$el.height();
 
-                    $el.imagePanAnimate();
-                })
-            })
-        },
-        animate:function(){
-            var $el=this;
-            var el=$el.get(0);
+          el.opt.cWidth=el.opt.cWidth?el.opt.cWidth:el.opt.oWidth;
+          el.opt.cHeight=el.opt.cHeight?el.opt.cHeight:el.opt.oHeight;
 
-            var scale = el.opt.minScale + (Math.random() * el.opt.maxScale);
+          el.opt.cWidth=$el.data("width")?$el.data("width"):el.opt.cWidth;
+          el.opt.cHeight=$el.data("height")?$el.data("height"):el.opt.cHeight;
 
-            var w=Math.round(el.opt.oWidth*scale);
-            var h=Math.round(el.opt.oHeight*scale);
+          el.opt.maxScale=el.opt.maxScale-el.opt.minScale;
 
-            //todo: Add height conditioning
-            //first verify width and height, than add css
-            var ratio;
-            if(w<el.opt.cWidth){
-                ratio = el.opt.cWidth/w;
-                w=el.opt.cWidth;
-                h = h*ratio;
-            }
+          $el.show().css({width:$el.width()*el.opt.startScale,height:$el.height()*el.opt.startScale, top:0, left:0});
 
-            if(h<el.opt.cHeight){
-                ratio = el.opt.cHeight/h;
-                w=w*ratio;
-                h = el.opt.cHeight;
-            }
+          var imgWrapper=$("<div/>").css({overflow:"hidden", width:el.opt.cWidth,height:el.opt.cHeight, position:"static", display:el.opt.display, margin:0});
+          $el.css({position:"static"}).wrap(imgWrapper);
+
+          $el.imagePanAnimate();
+        }).attr("src",$el.attr("src")+"?_="+new Date().getTime()).css({opacity:0}).hide();
+      })
+    },
+
+    animate:function(){
+      var $el=this;
+      var el=$el.get(0);
+
+      var scale = el.opt.minScale + (Math.random() * el.opt.maxScale);
+
+      var w=Math.round(el.opt.oWidth*scale);
+      var h=Math.round(el.opt.oHeight*scale);
+
+      var ratio;
+      if(w<el.opt.cWidth){
+        ratio = el.opt.cWidth/w;
+        w=el.opt.cWidth;
+        h = h*ratio;
+      }
+
+      if(h<el.opt.cHeight){
+        ratio = el.opt.cHeight/h;
+        w=w*ratio;
+        h = el.opt.cHeight;
+      }
 
 
-            var t= - Math.random()*((h-el.opt.cHeight));
-            var l= - Math.random()*((w-el.opt.cWidth));
-            var v= el.opt.velocity[0]+Math.random()*(el.opt.velocity[1]-el.opt.velocity[0]);
+      var t= - Math.random()*((h-el.opt.cHeight));
+      var l= - Math.random()*((w-el.opt.cWidth));
+      var v= el.opt.velocity[0]+Math.random()*(el.opt.velocity[1]-el.opt.velocity[0]);
 
-            $el.fadeTo(600,1,function(){$el.CSSAnimate({width:w, height:h, top:t, left:l},v,el.opt.type,function(){$el.imagePanAnimate()});});
+      $el.fadeTo(600,1,function(){$el.CSSAnimate({width:w, height:h, marginTop:t, marginLeft:l},v,el.opt.type,function(){$el.imagePanAnimate()});});
+    }
+  };
 
-        }
+  $.fn.CSSAnimate=function(opt, duration, type, callback){
+
+    //console.debug(opt);
+
+    if(!opt) return;
+
+    if(typeof duration=="function"){
+      callback=duration;
+    }
+    if(typeof type=="function"){
+      callback=type;
+    }
+    if(!duration)
+      duration=1000;
+
+    if(!type)
+      type="cubic-bezier(0.65,0.03,0.36,0.72)";
+
+    //http://cssglue.com/cubic
+    //  ease | linear | ease-in | ease-out | ease-in-out | cubic-bezier(<number>, <number>, <number>, <number>)
+
+    var el=this;
+
+    if($.browser.msie){
+      el.animate(opt,duration,callback);
+      return;
+    }
+
+    var sfx="";
+    var transitionEnd = "TransitionEnd";
+    if ($.browser.webkit) {
+      sfx="-webkit-";
+      transitionEnd = "webkitTransitionEnd";
+    } else if ($.browser.mozilla) {
+      sfx="-moz-";
+      transitionEnd = "transitionend";
+    } else if ($.browser.opera) {
+      sfx="-o-";
+      transitionEnd = "oTransitionEnd";
+    }
+
+    el.css(sfx+"transition-property","margin-top, margin-left, width, height");
+    el.css(sfx+"transition-duration",duration+"ms");
+    el.css(sfx+"transition-timing-function",type);
+
+    el.css(opt);
+
+    var endTransition = function(){
+      el.css(sfx+"transition","");
+      if(typeof callback=="function")
+        callback();
+      el.get(0).removeEventListener(transitionEnd,endTransition,true);
     };
-
-    $.fn.CSSAnimate=function(opt, duration, type, callback){
-
-        //console.debug(opt);
-
-        if(!opt) return;
-
-        if(typeof duration=="function"){
-            callback=duration;
-        }
-        if(typeof type=="function"){
-            callback=type;
-        }
-        if(!duration)
-            duration=1000;
-
-        if(!type)
-            type="cubic-bezier(0.65,0.03,0.36,0.72)";
-
-        //http://cssglue.com/cubic
-        //	ease | linear | ease-in | ease-out | ease-in-out | cubic-bezier(<number>, <number>, <number>, <number>)
-
-        var el=this;
-
-        if($.browser.msie){
-            el.animate(opt,duration,callback);
-            return;
-        }
-
-        var sfx="";
-        var transitionEnd = "TransitionEnd";
-        if ($.browser.webkit) {
-            sfx="-webkit-";
-            transitionEnd = "webkitTransitionEnd";
-        } else if ($.browser.mozilla) {
-            sfx="-moz-";
-            transitionEnd = "transitionend";
-        } else if ($.browser.opera) {
-            sfx="-o-";
-            transitionEnd = "oTransitionEnd";
-        }
-
-        el.css(sfx+"transition-property","all");
-        el.css(sfx+"transition-duration",duration+"ms");
-        el.css(sfx+"transition-timing-function",type);
-
-        el.css(opt);
-
-        var endTransition = function(){
-            el.css(sfx+"transition","");
-            if(typeof callback=="function")
-                callback();
-            el.get(0).removeEventListener(transitionEnd,endTransition,true);
-        };
-        el.get(0).addEventListener(transitionEnd, endTransition, true);
-    };
+    el.get(0).addEventListener(transitionEnd, endTransition, true);
+  };
 
 
-    $.fn.zoomPan=$.zoomPan.init;
-    $.fn.imagePanAnimate=$.zoomPan.animate;
+  $.fn.zoomPan=$.zoomPan.init;
+  $.fn.imagePanAnimate=$.zoomPan.animate;
 
 })(jQuery);
 
-function random(val){
-    return 1+Math.round(Math.random()*val);
+function random(from, to){
+  return from+Math.round(Math.random()*to);
 }
